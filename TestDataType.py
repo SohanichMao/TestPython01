6697956636f5b44f57cec13156db06d64a864876
SohanichMao <sophanic1111@gmail.com>
2024-07-20 11:35:08 +0700

    second commit2

--- TestDataType.py
+++ TestDataType.py
@@ -1,7 +1,23 @@
-str='Hello KakSamoun \n'
-print(str)
-print(str[0])
-print(str[2:5])
-print(str[2:])
-print(str*20)
-print(str+"From BBu")
+# str_text='Hello KakSamoun \n'
+# print(str_text)
+# print(str_text[0])
+# print(str_text[2:5])
+# print(str_text[2:])
+# print(str_text*20)
+# print(str_text+"From BBu")
+#
+#
+# #number
+# a=1_000_000
+# print(a)
+# print(type(a))
+# b=1254.2
+# print(type(b))
+# print(b)
+
+val1=int("10")
+val2=int("30")
+total=val1+val2
+print(type(total))
+print("TOTAL = " + str(total))
+print(len(str(total)))
